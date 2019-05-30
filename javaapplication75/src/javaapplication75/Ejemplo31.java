87521e1c189ad741596a6cf69e37062669e2872f
Juan pablo <jleon4811@gmail.com>
2019-05-30 11:24:16 -0500

    Subiendo trabajo en clase 2

--- javaapplication75/src/javaapplication75/Ejemplo31.java
+++ javaapplication75/src/javaapplication75/Ejemplo31.java
@@ -19,7 +19,7 @@ public class Ejemplo31 {
         Scanner entrada = new Scanner(System.in);
         Random aleatorios = new Random();
         int[] datos = new int[6];
-        String[] rangos = {"0-8", "8.1-12", "12.1-16", "16.1-19", 
+        String[] rangos = {"0-8", "8.1-12", "12.1-16", "16.1-19",
             "19.1-20", "20.1 - adelante"};
         boolean bandera = true;
         while (bandera == true) {
@@ -34,31 +34,27 @@ public class Ejemplo31 {
                     if (nota >= 12.1 && nota <= 16) {
                         datos[2] = datos[2] + 1;
                     } else {
-                        if (nota >= 12.1 && nota <= 16) {
+                        if (nota >= 16.1 && nota <= 19) {
                             datos[3] = datos[3] + 1;
                         } else {
-                            if (nota >= 16.1 && nota <= 19) {
+                            if (nota >= 19.1 && nota <= 20) {
                                 datos[4] = datos[4] + 1;
                             } else {
-                                if (nota >= 19.1 && nota <= 20) {
-                                    datos[5] = datos[5] + 1;
-                                } else {
-                                    datos[6] = datos[6] + 1;
-                                }
+                                datos[5] = datos[5] + 1;
                             }
                         }
                     }
                 }
             }
-            
+
             entrada.nextLine(); //Limpiamos buffer
             //preguntamos al usuario si quisere seguir ingresando notas
             System.out.println("Quiere seguir ingresando notas");
             String notas = entrada.nextLine();
             //validamos la nota para poder seguir con el ciclo
-            if(notas.equalsIgnoreCase("si")){
+            if (notas.equalsIgnoreCase("si")) {
                 bandera = true;
-            }else{
+            } else {
                 bandera = false;
             }
         }
